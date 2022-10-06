fad3256b5c5a4528d669f018a98a13dc7376d0a5
igottheim <108734347+igottheim@users.noreply.github.com>
2022-10-06 14:42:15 -0400

    Update about.js

--- src/about.js
+++ src/about.js
@@ -10,7 +10,8 @@ const About = () => {
             height: '90vh'
         }}
     >
-        <h1>About</h1>
+        <h1>TASKFORCE: ORGANIZE YOUR LIFE!
+          Are you tired of being disorganized around the house? Well look no further. TASKFORCE is here to help you copmlete that laundry lists of open items! Login on the next page, or create a username to get everything the app has to offer!</h1>
     </div>
   )
 }
