50276fdaa9fee60e6c078cf5ae6e8ae76645d699
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-10-06 16:43:32 -0400

    login and sign up update

--- src/UserForm.js
+++ src/UserForm.js
@@ -6,17 +6,22 @@ function UserForm({handleSubmit}){
     return(
       <>
     <Form onSubmit={(e)=> handleSubmit(e)}>
+      <div className= 'user-app-reg-1'>
+    <h1>Login Here:</h1>
+    <h1>
     <label>
-      Username:
-      <input type="text" name="name" />
+   
+      <input className='user-input6' type="text" name="name" placeholder="Username" />
     </label>
     <label>
-      Password:
-      <input type="password" name="name" />
+
+      <input className='user-input6' type="password" name="name" placeholder="Password"/>
     </label>
-    <Button className= "button" variant="primary" type="submit">
+    <Button className= "todo-button" variant="primary" type="submit">
         Login
       </Button>
+      </h1>
+      </div>
   </Form>
 
 
